c535e56ee5e3057a167667745b2b62e5695f9d42
joulongleu <6103334+joulongleu@users.noreply.github.com>
2022-08-11 09:48:17 +0800

    Add files via upload

---
+++ html5countrymapv4.2/mapdata.js
@@ -0,0 +1,186 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "800", //'700' or 'responsive'
+    background_color: "#8b1f1f",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    TWN1156: {
+      name: "Kaohsiung City",
+      description: " "
+    },
+    TWN1158: {
+      name: "Pingtung",
+      description: " "
+    },
+    TWN1160: {
+      name: "Tainan City",
+      description: " "
+    },
+    TWN1161: {
+      name: "Hsinchu City",
+      description: " "
+    },
+    TWN1162: {
+      name: "Hsinchu",
+      description: " "
+    },
+    TWN1163: {
+      name: "Yilan",
+      description: " "
+    },
+    TWN1164: {
+      name: "Keelung City",
+      description: " "
+    },
+    TWN1165: {
+      name: "Miaoli",
+      description: " "
+    },
+    TWN1166: {
+      name: "Taipei City",
+      description: " "
+    },
+    TWN1167: {
+      name: "New Taipei City",
+      description: " "
+    },
+    TWN1168: {
+      name: "Taoyuan",
+      description: " "
+    },
+    TWN1169: {
+      name: "Changhua",
+      description: " "
+    },
+    TWN1170: {
+      hover_color: "red",
+      name: "Chiayi",
+      description: "雲林縣斗六市保長路50-118號",
+      color: "#008000"
+    },
+    TWN1171: {
+      name: "Chiayi City",
+      description: " "
+    },
+    TWN1172: {
+      name: "Hualien",
+      description: " "
+    },
+    TWN1173: {
+      name: "Nantou",
+      description: " "
+    },
+    TWN1174: {
+      name: "Taichung City",
+      description: " "
+    },
+    TWN1176: {
+      hover_color: "red",
+      name: "Yunlin",
+      description: "嘉義縣六腳鄉六南村238號",
+      color: "#8b1f1f"
+    },
+    TWN1177: {
+      name: "Taitung",
+      description: " "
+    },
+    TWN3414: {
+      name: "Penghu",
+      description: " "
+    },
+    TWN3415: {
+      name: "Kinmen",
+      description: " "
+    },
+    TWN5128: {
+      name: "Lienchiang",
+      description: " "
+    }
+  },
+  locations: {
+    "0": {
+      lat: "23.503490",
+      lng: "120.25243",
+      name: "老家"
+    },
+    "1": {
+      lat: "23.70564305830822",
+      lng: "120.53147274251366",
+      name: "我家",
+      url: "http://joulong.hopto.org:8333/2022/08/02/%e6%99%ba%e6%85%a7%e5%b1%8b%e5%b1%95%e7%a4%ba/"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
